5350c99885c4ed195a95482af424a3544bd202bc
Andy Tertzakian <andy.tertzakian@gmail.com>
2016-11-02 10:56:18 -0700

    Finished Task Swapping part of algorithm

--- ksa.v
+++ ksa.v
@@ -13,7 +13,7 @@ typedef enum {state_init,
 					state_get_S_at_i,
 					state_wait_S_at_i,
 					state_get_j_and_S_at_j,
-					state_wait_S_at_sj,
+					state_wait_S_at_j,
 					state_update_S_at_i,
 					state_update_S_at_j,
 					state_done} state_type;
@@ -27,12 +27,22 @@ reg [7:0] q;
 reg wren;
 
 //datapath regs
-reg [7:0] i, S_at_i;
-reg [7:0] j, S_at_j;
-wire [23:0] Key [2:0];
-assign Key[0] = SW[7:0];
-assign Key[1] = {6'b0000000, SW[9:8]};
-assign Key[2] = 8'b00000000;
+reg [8:0] i, j;
+reg [7:0] S_at_i, S_at_j; 
+reg [7:0] temp_swap; //for swapping 
+
+//The values for the secret key
+parameter Key_Length = 3;
+// wire [7:0] Key [2:0];
+// assign Key[2] = SW[7:0];
+// assign Key[1] = {6'b0000000, SW[9:8]};
+// assign Key[0] = 8'b00000000;
+wire [23:0] Key;
+assign Key[23:10] = 1'b0;
+assign Key[9:0] = SW;
+
+//To indicate when the process has completed
+assign LEDR[7] = (state == state_done);
 
 // include S memory structurally
 s_memory u0(address, CLOCK_50, data, wren, q);
@@ -43,8 +53,8 @@ always_ff @(posedge CLOCK_50) begin
 	
 		state_init : begin 
 			
-			//Date initialization
-			i = 8'b0;	
+			//Data initialization
+			i = 9'b000000000;	
 			
 			//Memory control
 			wren  <= 1'b1;
@@ -55,37 +65,120 @@ always_ff @(posedge CLOCK_50) begin
 		end //End state_init
 		
 		state_fill : begin 
-				
-			//Data update
-			i = i + 1'b1;
-			
-			
+					
 			//Memory Control
 			wren    <= 1'b1;
 			data    <= i[7:0];
 			address <= i[7:0];
 			
+			//Data update
+			i = i + 1'b1;
+			
 			//Next state logic
-			state <= (i == 256) ? state_ : state_fill; //if i has reached 256, S is full
+			state <= (i == 256) ? state_get_S_at_i : state_fill; //if i has reached 256, S is full
 			
 			//Next State Setup
 			if(i == 256) begin		
-				i = 8'b00000000;
-				j = 8'b00000000;			
+				i = 9'b000000000;
+				j = 9'b000000000;			
 			end//End if
 			
 		end //End state_fill
 		
+		state_get_S_at_i: begin
 		
+			//Here we request the value in memory at location i
+		
+			//Memory Control
+			wren    <= 1'b0;
+			address <= i[7:0];
+			
+			//Next state logic
+			state <= state_wait_S_at_i;
+			
+		end //End get_S_at_i
+		
+		state_wait_S_at_i : begin
+			
+			//Here we just wait a cycle for the data from the S memory
+			//to be loaded into S_at_i
+			
+			//Next state logic
+			state <= state_get_j_and_S_at_j;
+			
+		end //End state_wait_S_at_i
+		
+		state_get_j_and_S_at_j : begin
+		
+			//Data update
+			S_at_i = q; //Set S_at_i to be the result of requesting the memory at location i
+			//j 		 = (j + S_at_i + Key[i % Key_Length]) % 9'b10000000; //Update the value of j according to the algorithm
+			
+			case(i%Key_Length) 
+				0: j = (j + S_at_i + Key[23:16])%9'b100000000;
+				1: j = (j + S_at_i + Key[15:8]) %9'b100000000;
+				2: j = (j + S_at_i + Key[7:0])  %9'b100000000;
+			endcase
+			
+			//Memory Control
+			temp_swap = S_at_i;
+			address  <= j[7:0];
+			
+			//Next state logic
+			state <= state_wait_S_at_j;
+		
+		end //End state_get_j_and_S_at_j
+		
+		state_wait_S_at_j : begin
+		
+			//Next state logic
+			state <= state_update_S_at_i;
+		
+		end //End state_wait_S_at_j
+		
+		state_update_S_at_i :  begin
+		
+			//Data update
+			S_at_j = q; //Set S_at_i to be the result of requesting the memory at location i
+			
+			//Memory Control
+			wren    <= 1'b1;   //prepare the memory to be written to for swapping
+			address <= i[7:0];      //set the location to be written to to i
+			data    <= S_at_j; //set the value to write to S_at_j (i.e. overwrite S_at_i with S_at_j)
+			
+			//Next state logic
+			state <= state_update_S_at_j;
+		
+		end //End state_update_S_at_i;
+		
+		state_update_S_at_j : begin
+			
+			//Memory control
+			wren    <= 1'b1;
+			address <= j[7:0];  
+			data    <= temp_swap; //replace S_at_j with the old value of S_at_i
+			
+			//Data update
+			i = i + 1'b1;
+			
+			//Next state logic
+			state <= (i < 256) ? state_get_S_at_i : state_done;
+		
+		end //End state_update_S_at_j
 		
 		state_done : begin 
+			
+			//Memory control
+			wren <= 1'b0; //No more writing to memory is required after finishing
 				
-			state <= state_done;
+			//Next state logic	
+			state <= state_done; //stay here forever after completion
 		
 		end //End State_done
 		
 		default : begin 
 		
+			i = 9'b000000000;
 			state <= state_init;
 			
 		end //End Default
